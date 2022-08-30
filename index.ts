75c056d132b39f6d810566df39c999709e75c863
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-30 18:03:14 -0300

    added cors basic implementation

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import express from "express";
-import { connectMongoDB } from "./src/database";
-import user from "./src/user/controller/route";
+import express from 'express';
+import { connectMongoDB } from './src/database';
+import user from './src/user/controller/route';
+const cors = require('cors');
 
 //Initialize .env
-require("dotenv").config();
+require('dotenv').config();
 
 //Initialize express
 const app = express();
@@ -11,12 +12,15 @@ const app = express();
 //Initialize json parser
 app.use(express.json());
 
+app.use(cors());
+
+//Connect to Mongo
 connectMongoDB();
 
 //Initialize app endpoints
-app.use("/api/user", user); //User
+app.use('/api/user', user); //User
 
 //Start to listen to server port
 app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on ${process.env.PORT}`);
+	console.log(`Server is listening on ${process.env.PORT}`);
 });
