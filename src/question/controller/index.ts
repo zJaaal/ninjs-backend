5718963b8a339376016643ced2f7cc753243f8c6
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-31 15:30:24 -0300

    Added partial functionality to review controller

--- src/question/controller/index.ts
+++ src/question/controller/index.ts
@@ -25,6 +25,7 @@ const findById = async (req: Request, res: Response) => {
 
 const list = async (req: Request, res: Response) => {
 	try {
+		//Here i should get the user progress to merge it
 		const questions = await Questions.list(req.body.page, req.body.difficult);
 		if (!questions.length) {
 			return res.status(404).json({
@@ -44,8 +45,38 @@ const list = async (req: Request, res: Response) => {
 		});
 	}
 };
+const review = async (req: Request, res: Response) => {
+	try {
+		const question = await Questions.findAnswersById(req.body.questionID);
+		if (!question) {
+			return res.status(404).json({
+				status: 'Error',
+				ErrorMessage: "Couldn't find any question"
+			});
+		}
 
+		const result = question.correctAnswer == req.body.answer;
+
+		//Here i should make the request to post progress to User Model
+
+		res.status(200).json({
+			status: 'Completed',
+			result: {
+				correct: result,
+				uid: req.body.uid,
+				explanation: question.explanation
+			}
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			status: 'Error',
+			ErrorMessage: 'Something went wrong please contact an admin'
+		});
+	}
+};
 export const QuestionsController = {
 	findById,
-	list
+	list,
+	review
 };
