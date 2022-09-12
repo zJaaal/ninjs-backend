6daee298376314e09f232b2def0c3f0010427058
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-12 15:56:30 -0300

    Added validation to dynamic key

--- src/question/utils/asyncFilter.ts
+++ src/question/utils/asyncFilter.ts
@@ -3,7 +3,10 @@ const asyncFilter = <DataType, FilterType>(
 	key: string,
 	param: FilterType
 ): Promise<DataType[]> => {
-	return new Promise(res => {
+	return new Promise((res, rej) => {
+		if (!data.some(x => typeof (x as any)[key] != 'undefined')) {
+			rej(`The property "${key}" doesn't exist on object`);
+		}
 		res(data.filter((x: DataType) => (x as any)[key] == param));
 	});
 };
--- src/question/utils/mergeArrays.ts
+++ src/question/utils/mergeArrays.ts
@@ -1,9 +1,15 @@
-const mergeArrays = <T = any>(
+const mergeArrays = <T>(
 	mainArray: any[],
 	toMergeArray: T[],
 	key: string
 ): Promise<T[]> => {
-	return new Promise(res => {
+	return new Promise((res, rej) => {
+		if (
+			!mainArray.some(x => typeof (x as any)[key] != 'undefined') ||
+			!toMergeArray.some(x => typeof (x as any)[key] != 'undefined')
+		) {
+			rej(`The property "${key}" should exist in the objects of both arrays`);
+		}
 		res(
 			mainArray.map(
 				mainData =>
