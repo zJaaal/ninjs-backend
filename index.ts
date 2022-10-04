c890bf108f0d4831a25ea9cb5e649fc26b22488d
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-04 13:57:16 -0300

    implemented cors

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const app = express();
 //Initialize json parser
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 //Connect to Mongo
 connectMongoDB();
